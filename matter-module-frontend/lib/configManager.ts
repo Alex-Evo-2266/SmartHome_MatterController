21f062e9ed91fdfd113ff5072eb87d807aeda442
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2026-04-11 14:00:56 +0300

    create config

--- matter-module-frontend/lib/configManager.ts
+++ matter-module-frontend/lib/configManager.ts
@@ -1,5 +1,6 @@
 import { promises as fs } from "fs"
 import yaml from "js-yaml"
+import path from "path"
 
 const CONFIG_PATH = "/app/config/config/config.yml"
 
@@ -23,10 +24,58 @@ export class ControllerConfig{
         })
     }
 
+    // async readConf(): Promise<unknown> {
+    //     const file = await fs.readFile(CONFIG_PATH, "utf8")
+    //     const data = yaml.load(file)
+    //     return data
+    // }
+
     async readConf(): Promise<unknown> {
-        const file = await fs.readFile(CONFIG_PATH, "utf8")
-        const data = yaml.load(file)
-        return data
+        try {
+            const file = await fs.readFile(CONFIG_PATH, "utf8");
+            return yaml.load(file);
+        } catch (err: unknown) {
+            if (typeof err === "object" && err && "code" in err && err.code === "ENOENT") {
+                // 🔹 создаём директории
+                await fs.mkdir(path.dirname(CONFIG_PATH), { recursive: true });
+
+                // 🔹 дефолтный конфиг (можешь изменить)
+                const defaultConfig = {
+                    mqtt: {
+                        url: "mqtt://localhost:1883",
+                        baseTopik: "matter",
+                        user: "",
+                        password: ""
+                    },
+
+                    matter:{
+                        storagePath: "./matter-storage",
+                        fabricLabel: "My Controller"
+                    },
+
+                    ble:{
+                        enabled: true,
+                        hciId: 0
+                    },
+
+                    wifi:{
+                        ssid: "",
+                        password: ""
+                    }
+                };
+
+                // 🔹 записываем файл
+                await fs.writeFile(
+                CONFIG_PATH,
+                yaml.dump(defaultConfig),
+                "utf8"
+                );
+
+                return defaultConfig;
+            }
+
+            throw err;
+        }
     }
 
     async saveConf(data: object): Promise<void> {
