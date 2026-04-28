5579520d51b3e7bcafcfae6265821c341df1ba5e
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2026-04-28 20:09:45 +0300

    event added and deleted

--- src/bridge/MatterMqttBridge.ts
+++ src/bridge/MatterMqttBridge.ts
@@ -52,6 +52,15 @@ export class MatterMqttBridge {
         
         await this.nodeManager.initNode(BigInt(nodeId));
 
+        const info = await this.nodeManager.getDeviceInfo(BigInt(nodeId));
+
+        await this.transport.publish("event/added", {
+          nodeId: nodeId.toString(),
+          pairedAt: new Date().toISOString(),
+          info
+        });
+
+
       }
       catch(e){
         this.publicError("error connect", String(e))
@@ -60,6 +69,11 @@ export class MatterMqttBridge {
 
     await this.transport.subscribe("delete", async (msg) => {
       if(!msg || typeof msg !== 'object' || !("nodeId" in msg) || typeof msg.nodeId !== "string")return
+
+      await this.transport.publish("event/deleted", {
+          nodeId: msg.nodeId,
+          pairedAt: new Date().toISOString()
+        });
 
       await this.nodeManager.removeNode(msg.nodeId);
     });
